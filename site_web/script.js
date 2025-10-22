aecf378010d852f2676cbd0bc185b4f41b2ea883
Marceau <marceau.leriche66@gmail.com>
2025-10-22 13:56:01 +0200

    fdbbdxb x

--- site_web/script.js
+++ site_web/script.js
@@ -3,10 +3,10 @@ let evan = document.getElementById("evan");
 evan.addEventListener('mouseover', (e) => {
     console.log("Bonjour");
 });
-let p17 = decodeURIComponent.getElementById("p17") ;
+let p17 = document.getElementById("p17") ;
 
 p17.addEventListener('mouseover', (e) => {
     console.log("Bonjour") ;
 } ) ;
-let p17 = document.getElementById("p1");
+let p17 = document.getElementById("p17");
  p17.addEventListener('mouseover')
